26f47fe28b855af2d0bb27e75c174da2b49ed7c6
Unknown-user-C <42628079+jangchiang@users.noreply.github.com>
2022-01-11 23:08:44 +0800

    cleanup_files done

--- cleanup_files.py
+++ cleanup_files.py
@@ -5,7 +5,22 @@ Cleanup_file
 """
 import os
 
+
+def main():
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        for x in filenames:
+            new_name = get_fixed_filename(x)
+            print("Change name from {} to {}".format(x, new_name))
+            full_name = os.path.join(directory_name, new_name)
+            os.rename(full_name, new_name)
+
+
 def get_fixed_filename(filename):
-    '''this function will fix from the space change to _ and TXT to txt'''
+    """this function will fix from the space change to _ and TXT to txt"""
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")  # replace and fix the file type and name
     return new_name
+
+
+if __name__ == '__main__':
+    main()
