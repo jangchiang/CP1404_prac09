4822f3e3aaf3da1140eccdc68c319f9e910f55f5
Unknown-user-C <42628079+jangchiang@users.noreply.github.com>
2022-01-12 11:48:40 +0800

    fix clean up file

--- cleanup_files.py
+++ cleanup_files.py
@@ -9,6 +9,10 @@ import os
 def main():
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
         for x in filenames:
             new_name = get_fixed_filename(x)
             print("Change name from {} to {}".format(x, new_name))
